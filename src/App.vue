9fec73e096ce4c7ececb0d76b403d315c137124e
Kaan <kaangokdemir01@gmail.com>
2020-08-07 20:23:38 +0300

    mixin usage

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template lang="pug">
-  #app
-    router-view.py-2.px-2
+#app.py-2.px-2
+  p.title {{ helloWorld }}
+  router-view
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Mixins } from 'vue-property-decorator'
+import { Hello, World } from '@/mixins/example'
 @Component
-export default class App extends Vue {}
+export default class App extends Mixins(Hello, World) {
+  get helloWorld () {
+    return this.hello + this.world
+  }
+}
 </script>
